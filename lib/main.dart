e3de29f3e82689f794ee2be31ea27ca74c8ce4d0
up2246840 <up2246840@myport.ac.uk>
2025-11-21 07:17:51 +0000

    Remove text widget and change other text widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sandwich Shop App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -54,6 +54,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // ignore: unused_field
   int _counter = 0;
 
   void _incrementCounter() {
@@ -103,13 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: <Widget>[const Text('Welcome to my shop:')],
         ),
       ),
       floatingActionButton: FloatingActionButton(
